7d4126e7a03415ab71801a103ed7dd4b174fc914
Hriday Bradoo <hriday.bradoo@kellogg.northwestern.edu>
2024-01-11 03:32:44 +0000

    Lab 4 sql - class 2

--- lab-4-2.sql
+++ lab-4-2.sql
@@ -1,6 +1,13 @@
 -- What are the first and last names of the players who 
 -- played for the 2020 Chicago Cubs?
 
+SELECT players.first_name, players.last_name 
+FROM players 
+INNER JOIN stats on players.id = stats.player_id
+INNER JOIN teams on stats.team_id = teams.id
+WHERE teams.year = 2020
+AND teams.name = "Chicago Cubs";
+
 -- Hint: combine WHERE clauses using AND, e.g.
 -- WHERE column1 = expression1
 -- AND column2 = expression2
--- lab-4-3.sql
+++ lab-4-3.sql
@@ -1,5 +1,11 @@
 -- Who hit the most home runs in 2019, and what team did they play for?
 
+SELECT players.first_name, players.last_name , teams.name, MAX(stats.home_runs)
+FROM players 
+INNER JOIN stats on players.id = stats.player_id
+INNER JOIN teams on stats.team_id = teams.id
+WHERE teams.year = 2019;
+
 -- Expected result:
 --
 -- +---------------+------------+-----------+-----------+
